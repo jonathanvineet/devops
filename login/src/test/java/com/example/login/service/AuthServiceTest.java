2a97785a45408abb3b6602ac6c395e2a7da23332
Vineet Jonathan C <cvineetjonathan@gmail.com>
2026-04-28 04:55:32 +0000

    test

--- login/src/test/java/com/example/login/service/AuthServiceTest.java
+++ login/src/test/java/com/example/login/service/AuthServiceTest.java
@@ -5,12 +5,10 @@ import com.example.login.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class AuthServiceTest {
 
     @Autowired
